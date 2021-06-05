aa8f98c7942f62d1b7cd602a6a20acc61c3a63d3
BurmistrovaEO <tigrettokat@mail.ru>
2021-06-05 13:51:31 +0300

    OpenMP lint fixed

--- modules/task_2/bourmistrova_e_trapez_integr_omp/trapez_integr_omp.h
+++ modules/task_2/bourmistrova_e_trapez_integr_omp/trapez_integr_omp.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Ekaterina Burmistrova
-#ifndef MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZOID_INTEGRAL_OMP_H_
-#define MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZOID_INTEGRAL_OMP_H_
+#ifndef MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZ_INTEGR_OMP_H_
+#define MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZ_INTEGR_OMP_H_
 #include <vector>
 #include <string>
 #include <functional>
@@ -8,8 +8,8 @@
 
 // std::vector<int> getRandomVector(int  sz);
 double SolveParallel(const std::vector<std::pair<int, int>>& bord,
-	std::function<double(double, double, double)> f);
+    std::function<double(double, double, double)> f);
 double SolveParallelSum(const std::vector<std::pair<int, int>>& bord,
-	std::function<double(double, double, double)> f);
+    std::function<double(double, double, double)> f);
 
-#endif  // MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZOID_INTEGRAL_OMP_H_
+#endif  // MODULES_TASK_2_BOURMISTROVA_E_TRAPEZ_INTEGR_OMP_TRAPEZ_INTEGR_OMP_H_
